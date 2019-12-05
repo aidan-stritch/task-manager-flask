4c118ba939ca82cd794bb926cecd82220e56b619
aidan-stritch <aidan.stritch@gmail.com>
2019-12-05 17:15:02 +0000

    updated origin

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-qoq2i.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+
+
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+                     
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))                          
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',
+            port=int('8080'),
+            debug=True)
